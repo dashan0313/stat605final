1bef5fecb8c03d1a347b14c22abdc4f1ad58f538
dashan0313 <hjs349960325@163.com>
2020-11-20 12:18:32 -0600

    fix bug on tsv cleaner

--- code/clean_review_tsv.sh
+++ code/clean_review_tsv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed 's/^.*","business_id":"//g' review.json | sed 's/","stars":/\t/g' | sed 's/"useful":.*,"text":/\t/g' | sed 's/","date":/"\t/g' | sed 's/}$//g' > review_clean.tsv
+sed 's/^.*","business_id":"//g' review.json | sed 's/","stars":/\t/g' | sed 's/,"useful":.*,"text":/\t/g' | sed 's/","date":/"\t/g' | sed 's/}$//g' > review_clean.tsv
 
